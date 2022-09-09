07fcf71e720e7d70955240507462bb9045b12c9f
Harry Law <53987565+h5law@users.noreply.github.com>
2022-09-09 20:17:17 +0100

    Implement custom directory walker

--- walk/walker.go
+++ walk/walker.go
@@ -4,92 +4,212 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
-	"path/filepath"
+	"os"
+	"sort"
 	"strings"
 
 	"github.com/fatih/color"
 	"github.com/spf13/viper"
 )
 
+const Line = "\u2502"      //│
+const LineRight = "\u251C" //├
+const Dash = "\u2500"      //─
+const Corner = "\u2514"    //└
+
+var lastDir bool
+
+type ByDirFirst []fs.DirEntry
+
+func (b ByDirFirst) Len() int {
+	return len(b)
+}
+
+func (b ByDirFirst) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+func (b ByDirFirst) Less(i, j int) bool {
+	if b[i].IsDir() && b[j].IsDir() {
+		// Both dirs go by name
+		return b[i].Name() < b[j].Name()
+	} else if b[i].IsDir() && !b[j].IsDir() {
+		// Put dirs first
+		return true
+	} else if !b[i].IsDir() && b[j].IsDir() {
+		return false
+	} else {
+		// Sort files by name
+		return b[i].Name() < b[j].Name()
+	}
+}
+
 func Walk(root string, dc, fc *int) error {
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			fmt.Printf("\"%s\" [error checking dir: %v]", path, err)
-			return filepath.SkipDir
+	// Open file at path
+	f, err := os.Open(root)
+	if err != nil {
+		msg := fmt.Sprintf("\"%s\" [error opening dir]", root)
+		return errors.New(msg)
+	}
+
+	// Increment for root
+	*dc += 1
+
+	// Print root path info
+	color.Set(color.Bold, color.FgBlue)
+	info, err := f.Stat()
+	if err != nil {
+		msg := fmt.Sprintf("\"%s\" [error checking path]", f.Name())
+		return errors.New(msg)
+	}
+	fmt.Printf("[ %s] %s\n", ConvertBytes(info.Size()), f.Name())
+	color.Unset()
+
+	if err := loopDirs(root, root, dc, fc); err != nil {
+		fmt.Println(err)
+	}
+
+	return nil
+}
+
+func loopDirs(base, root string, dc, fc *int) error {
+	// Get slice containing directory contents
+	dirs, err := readDir(root)
+	if err != nil {
+		msg := fmt.Sprintf("\"%s\" [error getting directory contents]", root)
+		return errors.New(msg)
+	}
+
+	for i, d := range dirs {
+		root = strings.TrimSuffix(root, "/")
+		path := root + "/" + d.Name()
+
+		if ignore := checkIgnore(path); ignore {
+			continue
 		}
 
-		// Skip anything matching ignore pattern
-		ignoreStr := viper.GetString("ignore")
-		ignore := strings.Split(ignoreStr, ",")
-		for _, v := range ignore {
-			if v != "" && strings.Contains(path, v) {
-				return nil
-			}
+		// Get file info
+		info, err := d.Info()
+		if err != nil {
+			msg := fmt.Sprintf("\"%s\" [error checking path]", d.Name())
+			return errors.New(msg)
 		}
 
 		// Get indent
-		shortPath := strings.TrimPrefix(path, root)
-		shortPath = strings.TrimPrefix(shortPath, "/")
-		indent := strings.Count(shortPath, "/") + 1
-		if path == root {
-			indent = 0
+		shortPath := strings.TrimPrefix(path, base)
+		indent := 0
+		if base == "." {
+			indent = strings.Count(shortPath, "/")
+		} else {
+			indent = strings.Count(shortPath, "/") + 1
 		}
 
 		// Get levels
 		levels := viper.GetUint("levels")
 		if uint(indent) > levels && levels > 0 {
-			return filepath.SkipDir
+			continue
 		}
 
+		// TODO Figure this out
 		/* Print tree chars
-		├ \u251C
-		─ \u2500
-		└ \u2514
+		│ Line
+		├ LineRight
+		─ Dash
+		└ Corner
 		*/
-		if indent == 1 {
-			fmt.Printf("%s%s ", "\u251C", strings.Repeat("\u2500", 2))
+		if indent == 1 && i != len(dirs)-1 {
+			fmt.Printf("%s%s ", LineRight, strings.Repeat(Dash, indent*4))
 		}
-		if indent > 1 {
-			fmt.Printf("%s%s ", "\u251C", strings.Repeat("\u2500", indent*4))
+		if indent == 1 && i == len(dirs)-1 {
+			lastDir = true
+			fmt.Printf("%s%s ", Corner, strings.Repeat(Dash, indent*4))
+		}
+		if !lastDir {
+			fmt.Printf("%s", strings.Repeat(Line+"     ", indent-1))
+		}
+		if indent > 2 && lastDir {
+			fmt.Printf("      %s", strings.Repeat(Line+"      ", indent-2))
+		} else if lastDir {
+			fmt.Printf(strings.Repeat("      ", indent-1))
+		}
+		if indent > 1 && i != len(dirs)-1 {
+			fmt.Printf("%s%s ",
+				LineRight,
+				strings.Repeat(Dash, 4),
+			)
+		}
+		if indent > 1 && i == len(dirs)-1 {
+			fmt.Printf("%s%s ",
+				Corner,
+				strings.Repeat(Dash, 4),
+			)
 		}
 
-		// Get fs.FileInfo from d
-		info, err := d.Info()
-		if err != nil {
-			fmt.Printf("\"%s\" [error getting path info]", path)
-			return filepath.SkipDir
-		}
-
-		// Check if dir
-		isDir := d.IsDir()
-		if isDir {
-			color.Set(color.Bold, color.FgBlue)
-			defer color.Unset()
-		}
-
-		// Increment counts
-		if isDir {
+		// Increment counters and set color
+		if d.IsDir() {
 			*dc += 1
+			color.Set(color.Bold, color.FgBlue)
 		} else {
 			*fc += 1
 		}
 
-		// Convert filesize
-		size := ConvertBytes(info.Size())
+		// Print file info
+		fmt.Printf("[ %s] %s\n", ConvertBytes(info.Size()), info.Name())
+		color.Unset()
 
-		name := d.Name()
-		if path == root {
-			name = path
+		// Loop through next dir
+		if d.IsDir() {
+			if err := loopDirs(base, root+"/"+d.Name(), dc, fc); err != nil {
+				return err
+			}
 		}
-		fmt.Printf("[ %s] %s\n", size, name)
-		return nil
-	})
-
-	if err != nil {
-		return errors.New(fmt.Sprintf("\"%s\" [error: %s]", root, err))
 	}
 
 	return nil
+}
+
+// return true to ignore false to keep
+func checkIgnore(path string) bool {
+	// Skip anything matching ignore pattern
+	ignoreStr := viper.GetString("ignore")
+	ignore := strings.Split(ignoreStr, ",")
+	for _, v := range ignore {
+		if v != "" && strings.Contains(path, v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func readDir(path string) ([]fs.DirEntry, error) {
+	// Open file at path
+	f, err := os.Open(path)
+	if err != nil {
+		msg := fmt.Sprintf("\"%s\" [error opening dir]", path)
+		return nil, errors.New(msg)
+	}
+
+	// Get directory contents
+	dirs, err := f.ReadDir(-1)
+	f.Close()
+	if err != nil {
+		msg := fmt.Sprintf("\"%s\" [error getting directory contents]", path)
+		return nil, errors.New(msg)
+	}
+
+	// Sort directory contents
+	dirsFirst := viper.GetBool("dirs-first")
+	if dirsFirst {
+		sort.Sort(ByDirFirst(dirs))
+	} else {
+		sort.Slice(dirs, func(i, j int) bool { return dirs[i].Name() < dirs[j].Name() })
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return dirs, nil
 }
 
 func ConvertBytes(bytes int64) string {
